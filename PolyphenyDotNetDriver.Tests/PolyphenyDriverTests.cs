d3023e62cafea56e2ac732b538aae2ce3e74eca8
malikrafsan <pro.malikakbar2357@gmail.com>
2024-06-02 11:30:43 +0700

    test: add test for execute non query command

--- PolyphenyDotNetDriver.Tests/PolyphenyDriverTests.cs
+++ PolyphenyDotNetDriver.Tests/PolyphenyDriverTests.cs
@@ -23,4 +23,27 @@ public class PolyphenyDriverTests
         await connection.Ping();
         connection.Close();
     }
+    
+    [Test]
+    public async Task ShouldExecuteNonQuery()
+    {
+        var connection = new PolyphenyConnection("localhost:20590,pa:");
+        connection.Open();
+        var command = new PolyphenyCommand().WithConnection(connection).WithCommandText("DROP TABLE IF EXISTS test");
+        var result = command.ExecuteNonQuery();
+        Console.WriteLine("result: " + result);
+        Assert.That(result, Is.EqualTo(1));
+        
+        command = command.WithCommandText("CREATE TABLE test (id INT PRIMARY KEY, name VARCHAR(255))");
+        result = command.ExecuteNonQuery();
+        Console.WriteLine("result: " + result);
+        Assert.That(result, Is.EqualTo(1));
+        
+        command = command.WithCommandText("DROP TABLE IF EXISTS test");
+        result = command.ExecuteNonQuery();
+        Console.WriteLine("result: " + result);
+        Assert.That(result, Is.EqualTo(1));      
+        
+        connection.Close();
+    }
 }
